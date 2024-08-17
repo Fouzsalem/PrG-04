627314507795047cb8db5f401ac3f15b84e4d9bc
reham220 <411201881@qu.edu.sa>
2024-08-17 06:21:03 +0300

    new update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientsController;
+use App\Http\Controllers\ServicesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/insert', function () {
+    return view('input');
+});
+
+
+
+Route::get('/view',[ClientsController::class,'view']);
+
+Route::post('/insert',[ClientsController::class,'insert']);
+
+
+Route::get('/edit/{id}',[ClientsController::class,'edit']);
+
+Route::put('/update/{id}',[ClientsController::class,'update']);
+
+Route::delete('/del/{id}',[ClientsController::class,'del']);
+
+
+
+
+Route::get('/view2',[ServicesController::class,'serv.view']);
+
+Route::post('/insert2',[ServicesController::class,'serv.insert']);
+
+Route::get('/edit2/{id}',[ServicesController::class,'serv.edit']);
+
+Route::put('/update2/{id}',[ServicesController::class,'serv.update']);
+
+Route::delete('/del2/{id}',[ServicesController::class,'serv.del']);
